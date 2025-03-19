324e166bcd3cca63a185271a33d5e80df2701543
nayeemislamnishad <159826869+nayeemislamnishad@users.noreply.github.com>
2025-03-20 02:09:32 +0600

    Update scriptmaster.js

--- scriptmaster.js
+++ scriptmaster.js
@@ -3,6 +3,6 @@ const columns = 1;
 let timeLeft = 1200;
 
 
-const presetAnswers = "acdacddaacdacddaacdacddaacdacddaacdacdda"; //BAU= 7-8 Math
+const presetAnswers = "cabbacadbcbadbbacbababda"; //BAU= 7-8 Math
 const startQuestionNumber = 76; // এখান থেকে প্রশ্ন শুরু হবে
 
